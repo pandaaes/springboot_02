f85f212c7d94f716698d912a4e46ce52ee85f6ed
Liujh <3362564235@qq.com>
2023-03-23 14:42:47 +0800

    first commit

---
+++ src/test/java/com/liujh/itheima/springboot_02/Springboot02ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.liujh.itheima.springboot_02;
+
+import org.junit.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
